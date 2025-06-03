19856e1d98f8da3f41fd893d1f8a64e864c0eab1
dayano74 <kiixxn00@gmail.com>
2025-06-03 15:56:39 +0900

    update

--- exam03/ft_printf/main.c
+++ exam03/ft_printf/main.c
@@ -3,18 +3,38 @@
 int main(void)
 {
 	int ret;
-	// printf("%d\n", ft_strlen("asdf"));
-	// ret = ft_printf("hello\n");
-	// ret = ft_printf("adsf %s\n", "hello");
-	// printf("ret = %d\n", ret);
-	// ft_printf("%s\n", "hello");
-	// ft_printf("Hello", "World!");
-	// ret = ft_printf("%d\n", 2147483647);
-	// printf("ret = %d\n", ret);
-	// ret = ft_printf("%d\n", -2147483648);
-	// printf("ret = %d\n", ret);
-	ret = ft_printf("%d\n", 10);
+
+	ret = ft_printf("hello\n");
 	printf("ret = %d\n", ret);
+	ret = printf("hello\n");
+	printf("ret = %d\n", ret);
+
+
+	ret = ft_printf("adsf %s\n", "hello");
+	printf("ret = %d\n", ret);
+	ret = printf("adsf %s\n", "hello");
+	printf("ret = %d\n", ret);
+
+	ret = ft_printf("%d\n", 123);
+	printf("ret = %d\n", ret);
+	ret = printf("%d\n", 123);
+	printf("ret = %d\n", ret);
+
+	ret = ft_printf("%d\n", -456);
+	printf("ret = %d\n", ret);
+	ret = printf("%d\n", -456);
+	printf("ret = %d\n", ret);
+
+	ret = ft_printf("%d\n", 2147483647);
+	printf("ret = %d\n", ret);
+	ret = printf("%d\n", 2147483647);
+	printf("ret = %d\n", ret);
+
+	ret = ft_printf("%d\n", -2147483648);
+	printf("ret = %d\n", ret);
+	ret = printf("%d\n", (int)-2147483648);
+	printf("ret = %d\n", ret);
+
 	ret = ft_printf("%x\n", 10);
 	printf("ret = %d\n", ret);
 	ret = printf("%x\n", 10);
